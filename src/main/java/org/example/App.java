962cecfa9de537cb8256dc3295051968bea198b8
Arturo <you@example.com>
2024-05-11 03:55:53 -0500

    cambios en la rama dev-login Comentarios

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ import org.example.models.Varon;
 public class App 
 {
     public static void main( String[] args ) {
-    System.out.println("Cambios 01 en la rama Dev");
+    //Cambios en al rama dev-login #1
     System.out.println("Cambios generados en la rama dev-login");
     }
 }
