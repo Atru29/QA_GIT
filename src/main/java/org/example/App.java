3889e719e3db11f4fa429410136484a2351402a7
Arturo <you@example.com>
2024-05-11 04:18:07 -0500

    cambios dev 0002

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,6 @@ import org.example.models.Varon;
 public class App 
 {
     public static void main( String[] args ) {
-    System.out.println("Cambios generados en la rama dev-login");
         //Modificado en al web #1
         System.out.println("Cambios 01 en la rama Dev");
         //Modificado en al web #2
